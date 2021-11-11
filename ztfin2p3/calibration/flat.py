0d1150b1499d3e8b24a9a2caee205b3f4237e43e
MickaelRigault <mick.rigault@gmail.com>
2021-11-11 15:15:35 +0100

    adding flat tools

--- ztfin2p3/calibration/flat.py
+++ ztfin2p3/calibration/flat.py
@@ -1,3 +1,71 @@
+
 """ library to build the ztfin2p3 pipeline screen flats """
+import os
+import pandas
+from .io import FLAT_DIR 
+
+class RawFlatMeta( object ):
+    """ 
+    Access the yearly IRSA metadata associated to the raw flat data.
+    
+    Usage:
+    ------
+    ```
+    data = RawFlatMeta.get_yearly_metadata(2020)
+    ```
+    This will download the metadata if not already stored locally 
+    and return the corresponding dataframe.
+    If this needs to download the data it may take couple of minutes.
+    Data stored as parquet.
+
+    """
+    # ============== #
+    #  Core Methods  #
+    # ============== #
+    @classmethod
+    def get_yearly_metadata(cls, year, force_dl=False, **kwargs):
+        """ """
+        filepath = cls.get_yearly_metadatafile(year)
+        if force_dl or not os.path.isfile(filepath):
+            cls.build_yearly_metadata(year)
+
+        return pandas.read_parquet(filepath, **kwargs)
+
+    # ============== #
+    #  INTERNAL      #
+    # ============== #
+    @staticmethod
+    def get_yearly_metadatafile(year):
+        """ """
+        return os.path.join(FLAT_DIR, "meta", f"rawflat_metadata{year:04d}.parquet")
+
+    @classmethod
+    def build_yearly_metadata(cls, year):
+        """ """
+        from astropy import time
+        from ztfquery import query
+        zquery = query.ZTFQuery()
+
+        start = time.Time(f"{year}-01-01")
+        end = time.Time(f"{year}-12-31")
+
+        zquery.load_metadata("raw", sql_query=f"obsjd between {start.jd} and {end.jd} and imgtypecode = 'f'")
+        if len(zquery.data)>10:
+            zquery.data.to_parquet(cls.get_yearly_metadatafile(year))
+            
+        return 
+    
+
+        
 
 
+    
+class FlatBuilder( object ):
+
+    @classmethod
+    def from_rawfiles(self, rawfiles):
+        """ """
+        
+    
+    
+        
--- ztfin2p3/io.py
+++ ztfin2p3/io.py
@@ -5,6 +5,16 @@ from ztfquery.io import LOCALSOURCE
 BASESOURCE = os.path.join(LOCALSOURCE, "ztfin2p3")
 
 
+#########################
+#                       #
+#                       #
+#    CALIBRATION        #
+#                       #
+#                       #
+#########################
+BIAS_DIR = os.path.join(BASESOURCE, "cal/bias")
+FLAT_DIR = os.path.join(BASESOURCE, "cal/flat")
+STARFLAT_DIR = os.path.join(BASESOURCE, "cal/starflat")
 
 # =========== #
 #  BIAS       #
@@ -15,7 +25,7 @@ def get_daily_biasfile(yyyy, mm, dd, ccdid):
     
     """
     filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}{dd:02d}_000000_bi_c{ccdid:02d}_bias.fits" 
-    return os.path.join(BASESOURCE, "cal/bias", f"{yyyy:04d}",f"{mm:02d}{dd:02d}", 
+    return os.path.join(BIAS_DIR, f"{yyyy:04d}",f"{mm:02d}{dd:02d}", 
                         filestructure)
 
 def get_weekly_biasfile(yyyy, www, ccdid):
@@ -24,7 +34,7 @@ def get_weekly_biasfile(yyyy, www, ccdid):
     
     """
     filestructure = f"ztfin2p3_{yyyy:04d}{www:03d}_000000_bi_c{ccdid:02d}_bias.fits" 
-    return os.path.join(BASESOURCE, "cal/bias", f"{yyyy:04d}",f"{www:03d}", 
+    return os.path.join(BIAS_DIR, f"{yyyy:04d}",f"{www:03d}", 
                         filestructure)
 
 def get_monthly_biasfile(yyyy, mm, ccdid):
@@ -33,7 +43,7 @@ def get_monthly_biasfile(yyyy, mm, ccdid):
     
     """
     filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}_000000_bi_c{ccdid:02d}_bias.fits" 
-    return os.path.join(BASESOURCE, "cal/bias", f"{yyyy:04d}",f"{mm:02d}", 
+    return os.path.join(BIAS_DIR, f"{yyyy:04d}",f"{mm:02d}", 
                         filestructure)
 
 # =========== #
@@ -53,7 +63,7 @@ def get_daily_flatfile(yyyy, mm, dd, ccdid, filtername, ledid=None):
         ledid = 0
 
     filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}{dd:02d}_000000_{filtername}_c{ccdid:02d}_l{ledid:02d}_flat.fits" 
-    return os.path.join(BASESOURCE, "cal/flat", f"{yyyy:04d}",f"{mm:02d}{dd:02d}", 
+    return os.path.join(FLAT_DIR, f"{yyyy:04d}",f"{mm:02d}{dd:02d}", 
                         filestructure)
 
 def get_weekly_flatfile(yyyy, www, ccdid, filtername, ledid=None):
@@ -74,7 +84,7 @@ def get_weekly_flatfile(yyyy, www, ccdid, filtername, ledid=None):
         ledid = 0
     
     filestructure = f"ztfin2p3_{yyyy:04d}{www:03d}_000000_{filtername}_c{ccdid:02d}_l{ledid:02d}_flat.fits" 
-    return os.path.join(BASESOURCE, "cal/flat", f"{yyyy:04d}",f"{www:03d}", 
+    return os.path.join(FLAT_DIR, f"{yyyy:04d}",f"{www:03d}", 
                         filestructure)
 
 def get_monthly_flatfile(yyyy, mm, ccdid, filtername, ledid=None):
@@ -93,7 +103,7 @@ def get_monthly_flatfile(yyyy, mm, ccdid, filtername, ledid=None):
         ledid = 0
     
     filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}_000000_{filtername}_c{ccdid:02d}_l{ledid:02d}_flat.fits" 
-    return os.path.join(BASESOURCE, "cal/flat", f"{yyyy:04d}",f"{mm:02d}", 
+    return os.path.join(FLAT_DIR, f"{yyyy:04d}",f"{mm:02d}", 
                         filestructure)
 
 # =========== #
@@ -105,5 +115,25 @@ def get_monthly_starflatfile(yyyy, mm, ccdid, filtername):
     format: cal/starflat/yyyy/mm/ztfin2p3_yyyymm_000000_filtername_ccdid_starflat.fits
     """
     filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}_000000_{filtername}_c{ccdid:02d}_starflat.fits" 
-    return os.path.join(BASESOURCE, "cal/starflat", f"{yyyy:04d}",f"{mm:02d}", 
+    return os.path.join(STARFLAT_DIR, f"{yyyy:04d}",f"{mm:02d}", 
                         filestructure)
+
+#########################
+#                       #
+#                       #
+#    SCIENCE IMAGE      #
+#                       #
+#                       #
+#########################
+def get_sciencefile(yyyy, mm, dd, fracday, field, filtername, ccdid, qid):
+    """ 
+    Following the same format as main this's IPAC. Only changing
+    ztf->ztfin2p3
+
+    e.g. ztfin2p3/sci/2018/0221/328009/ztfin2p3_20180221328009_700426_zg_c01_o_q1_sciimg.fits'
+
+    """
+    filestructure = f"ztfin2p3_{yyyy:04d}{mm:02d}{dd:02d}{fracday:06d}_{field:06d}_{filtername}_c{ccdid:02d}_o_q{qid:1d}_sciimg.fits"
+    return os.path.join(BASESOURCE, "sci", f"{yyyy:04d}",f"{mm:02d}{dd:02d}", f"{fracday:06d}",
+                        filestructure)
+    

fe40deaae06854d2c0b48b9b5057f074e4fcddc5
MickaelRigault <mick.rigault@gmail.com>
2021-11-13 14:59:06 +0100

    generalizing

--- ztfin2p3/calibration/flat.py
+++ ztfin2p3/calibration/flat.py
@@ -33,10 +33,83 @@ class RawFlatMeta( object ):
         return pandas.read_parquet(filepath, **kwargs)
 
     @classmethod
-    def get_yearly_zquery(cls, year, force_dl=False, daterange=[None,None]):
+    def get_metadata(cls, date, format=None):
+        """ """
+        if date is None:
+            raise ValueError("date cannot be None, could be string, float, or list of 2 strings")
+        if not hasattr(date, "__iter__"): # int/float given, convert to string
+            date = str(date)
+            
+        if type(date) is str and len(date) == 4:
+            return cls.get_yearly_metadata(date)
+        elif type(date) is str:
+            from ..utils.tools import parse_singledate
+            start, end = parse_singledate(date) # -> start, end
+        else:
+            from astropy import time 
+            start, end = time.Time(date, format=format).datetime
+        # 
+        # Now we have start and end in datetime format.
+        if start.year == end.year:
+            data = cls.get_yearly_metadata(start.year)
+        else:
+            delta_year = end.year-start.year
+            data = pandas.concat([cls.get_yearly_metadata(start.year+i) for i in range(delta_year+1)])
+                
+        datecol = data["obsdate"].astype('datetime64')
+        return data[datecol.between(start.isoformat(), end.isoformat())]
+    
+                
+    @classmethod
+    def get_zquery(cls, date, force_dl=False):
         """ """
         from ztfquery import query
+        import datetime
+
+        if date is None:
+            raise ValueError("date cannot be None, could be string, float, or list of 2 strings")
+        if not hasattr(date, "__iter__"): # int/float given, convert to string
+            date = str(date)
+            
+        if type(date) is str:
+            if len(date) == 4:
+                data = cls.get_yearly_metadata(date)
+            else:
+                from ..utils.tools import parse_singledate
+                
+                start, end = parse_singledate(date)
+                if start.year == end.year:
+                    data = cls.get_yearly_metadata(start.year)
+                else:
+                    delta_year = end.year-start.year
+                    data = pandas.concat([cls.get_yearly_metadata(start.year+i) for i in range(delta_year+1)])
+                
+                datecol = data["obsdate"].astype('datetime64')
+                data = data[datecol.between(start.isoformat(), end.isoformat())]
+        
+                
+                
+        
+        
+
+        
+
+
+        
         data = cls.get_yearly_metadata(year, force_dl=force_dl)
+        # - DateRange
+        if daterange is not None:
+            dstart, dend = daterange
+            if dstart is not None and dend is not None:
+                data = data[data["obsdate"].between(dstart,dend)]
+            elif dstart is not None:
+                data = data[data["obsdate"]>=dstart]
+            elif dend is not None:
+                data = data[data["obsdate"]<=dend]
+            else:
+                # both dstart and dend are None
+                pass
+
         return query.ZTFQuery(data, "raw")
         
 

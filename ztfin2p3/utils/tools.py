a65dc5dd88ed97c38781055d120c486ee8a8b957
MickaelRigault <mick.rigault@gmail.com>
2021-11-14 15:05:53 +0100

    flat and bias metadata

--- ztfin2p3/utils/tools.py
+++ ztfin2p3/utils/tools.py
@@ -2,6 +2,7 @@
 def parse_singledate(date):
     """ parse the input 'date' into a datetime start and end
     date could have the following format:
+        - yyyy: get  the full yearx
         - yyyymm: get the full month 
                  (stars at day 1, ends day 1 next month)
         - yyyywww: get the full calendar week
@@ -11,7 +12,11 @@ def parse_singledate(date):
     """
     import datetime
     date = str(date)
-    if len(date) == 6: # year+month
+    if len(date) == 4: # year
+        date_start = datetime.date.fromisoformat(f"{date}-01-01")
+        date_end   = datetime.date.fromisoformat(f"{int(date)+1}-01-01")
+        
+    elif len(date) == 6: # year+month
         from calendar import monthrange
         year, month = int(date[:4]),int(date[4:])
         date_start = datetime.date.fromisoformat(f"{year}-{month:02d}-01")

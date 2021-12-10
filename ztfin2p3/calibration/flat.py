ef560867203d4d1e7764d4f84f8172df2ee05e48
MickaelRigault <mick.rigault@gmail.com>
2021-12-10 15:20:29 +0100

    cleaning for bias/flat similarities

--- ztfin2p3/calibration/flat.py
+++ ztfin2p3/calibration/flat.py
@@ -221,76 +221,20 @@ class FlatFocalPlane( _FocalPlane_ ):
 #                      #
 #   Flat Builder       #
 #                      #
-# ==================== #    
-class FlatBuilder( object ): # /day /week /month
-
-    def __init__(self, rawflatcollection):
-        """ """
-        self.set_imgcollection(rawflatcollection)
-
-    
-    # ============== #
-    #  I/O           # 
-    # ============== #
-    @classmethod
-    def from_rawfiles(cls, rawfiles, **kwargs):
-        """ """
-        from ztfimg import raw
-        flatcollection = raw.RawFlatCCDCollection.from_filenames(rawfiles, **kwargs)
-        return cls(flatcollection)
-
-    def to_fits(self, fileout, header=None, overwrite=True):
-        """ Store the data in fits format """
-
-        from astropy.io import fits
-        if header is None:
-            if not self.has_header():
-                raise AttributeError("no header set and no header given.")
-            header = self.header
-
-        dirout = os.path.dirname(fileout)
-        if not os.path.isdir(dirout):
-            os.makedirs(dirout, exist_ok=True)
-
-        fits.writeto(fileout, self.data, header=header,
-                         overwrite=overwrite, **kwargs)
-        
-    # ============== #
-    #  Methods       # 
-    # ============== #
-    # -------- # 
-    #  SETTER  #
-    # -------- #
-    def set_imgcollection(self, imgcollection):
-        """ """
-        self._imgcollection = imgcollection
-
-    def set_data(self, data):
-        """ """
-        self._data = data
-
-    def set_header(self, header):
-        """ """
-        self._header = header
-        
+# ==================== #
+from .builder import CalibrationBuilder
+class FlatBuilder( CalibrationBuilder ): 
     # -------- # 
     # BUILDER  #
     # -------- #
     def build(self, corr_nl=True, corr_overscan=True, clipping=True,
                   set_it=False, inclheader=True, **kwargs):
         """ """
-        prop = {**dict(corr_overscan=corr_overscan, corr_nl=corr_nl, clipping=True),
-                **kwargs}
-        data = self.imgcollection.get_data_mean(**prop)
-        if inclheader:
-            header = self.build_header()
-        else:
-            header = None
-        if set_it:
-            self.set_data(data)
-            self.set_header(header)
-            
-        return data, header
+        return super().build(corr_nl=corr_nl,
+                                 corr_overscan=corr_overscan,
+                                 clipping=clipping,
+                                 set_it=set_it, inclheader=inclheader,
+                                 **kwargs)
 
     def build_header(self, keys=None, refid=0, inclinput=False):
         """ """
@@ -318,37 +262,3 @@ class FlatBuilder( object ): # /day /week /month
             
         return newheader
     
-    # ============== #
-    #  Properties    # 
-    # ============== #
-    @property
-    def imgcollection(self):
-        """  """
-        if not hasattr(self, "_imgcollection"):
-            return None
-        
-        return self._imgcollection
-    
-    @property
-    def data(self):
-        """ """
-        if not hasattr(self, "_data"):
-            return None
-        
-        return self._data
-
-    def has_data(self):
-        """ """
-        return self.data is not None
-    
-    @property
-    def header(self):
-        """ """
-        if not hasattr(self, "_header"):
-            return None
-        
-        return self._header
-    
-    def has_header(self):
-        """ """
-        return self.header is not None
